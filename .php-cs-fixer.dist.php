ed8b71a8e5976a56d218fdd04a199964c17b11a0
Chris Smith <chris.smith@widerplan.com>
2026-06-05 16:05:41 +0100

    Update PHP-CS-Fixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,6 +4,17 @@ return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR2' => true,
+        '@PHP54Migration' => true,
+        '@PHP56Migration:risky' => true,
+        '@PHP70Migration' => true,
+        '@PHP70Migration:risky' => true,
+        '@PHP71Migration' => true,
+        '@PHP71Migration:risky' => true,
+        '@PHP73Migration' => true,
+        '@PHP74Migration' => true,
+        '@PHP74Migration:risky' => true,
+        '@PHP80Migration' => true,
+        '@PHP80Migration:risky' => true,
         'list_syntax' => ['syntax' => 'long'],
         'native_constant_invocation' => true,
         'native_function_casing' => true,
@@ -13,6 +24,8 @@ return (new PhpCsFixer\Config())
         'no_alias_functions' => true,
         'no_extra_blank_lines' => true,
         'ordered_imports' => true,
+        'declare_strict_types' => false,
+        'use_arrow_functions' => false,
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
